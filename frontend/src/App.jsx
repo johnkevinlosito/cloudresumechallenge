86155547c8e439276dd0b17ad88dd31b2dfc787c
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2023-08-17 23:48:10 +0800

    feat: display visitor count

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [visitorCount, setVisitorCount] = useState(0);
+  useEffect(() => {
+    // fetch data from api
+    const fetchData = async () => {
+      const response = await fetch(import.meta.env.VITE_API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      const count = JSON.parse(data.body).visitor_count;
+      setVisitorCount(count);
+    };
+    fetchData();
+  }, []);
+
   return (
     <main className="font-firasans hyphens-manual">
       <div className="p-6 mx-auto page max-w-2xl print:max-w-letter md:max-w-letter md:h-letter xsm:p-8 sm:p-9 md:p-16 print:bg-white bg-gray-100">
@@ -6,6 +25,18 @@ function App() {
           <h1 className="text-2xl font-bold text-primary pb-px">
             JOHN KEVIN LOSITO
           </h1>
+          <div className="print:hidden flex items-center gap-2 text-gray-700">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path d="M15.5 2A1.5 1.5 0 0014 3.5v13a1.5 1.5 0 001.5 1.5h1a1.5 1.5 0 001.5-1.5v-13A1.5 1.5 0 0016.5 2h-1zM9.5 6A1.5 1.5 0 008 7.5v9A1.5 1.5 0 009.5 18h1a1.5 1.5 0 001.5-1.5v-9A1.5 1.5 0 0010.5 6h-1zM3.5 10A1.5 1.5 0 002 11.5v5A1.5 1.5 0 003.5 18h1A1.5 1.5 0 006 16.5v-5A1.5 1.5 0 004.5 10h-1z" />
+            </svg>
+
+            <span className="text-sm">{visitorCount} views</span>
+          </div>
         </header>
         <div className="md:columns-2 print:columns-2 gap-x-[2.1875rem] md:h-letter-col print:h-letter-col col-fill-auto">
           <section className="mt-8 first:mt-0">
